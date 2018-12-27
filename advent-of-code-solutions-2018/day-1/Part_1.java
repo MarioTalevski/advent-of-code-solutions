ef2650b68e38975ce3fa0c3caa2318d7fa04bb1d
Mario Talevski <mario.talevski@gmail.com>
2018-12-27 15:10:39 +0100

    Add files via upload

---
+++ advent-of-code-solutions-2018/day-1/Part_1.java
@@ -0,0 +1,22 @@
+package day_1_chronal_calibration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Part_1 {
+	
+	public static void main(String[] args) throws IOException {
+		List<String> inputList = Files.readAllLines(Paths.get("C:\\advent-of-code\\input.txt"));
+		int sum = 0;
+		
+        for(String input : inputList) {
+        	sum = sum + Integer.parseInt(input.replaceAll("[\\D&&[^,-]]", ""));	        	
+        }
+        
+        System.out.println(sum);
+       
+	}
+
+}
